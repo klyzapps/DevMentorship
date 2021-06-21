1e7999ce7284ab7228674be6668928aec0e7aa77
Wiktor Klyz <klyzwiktor@gmail.com>
2021-06-21 12:41:26 -0500

    Started working on the classes for goals

---
+++ goals.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+class ActionItem:
+    def updateAction(self, action)
+        self.action = action
+
+    def updateDueDate(self, dueDate)
+        self.dueDate = dueDate
+
+
